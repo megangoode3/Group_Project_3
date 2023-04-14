4df5e13c8e4141a81a8bf6f0834933a68cbe2097
Bennett <bennettnorth@gmail.com>
2023-04-13 21:57:50 -0500

    commit

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ Base = automap_base()
 Base.prepare(autoload_with=engine)
 
 # Save reference to the table
-Passenger = Base.classes.passenger
+Flight = Base.classes.flight_db
 
 #################################################
 # Flask Setup
@@ -33,52 +33,34 @@ app = Flask(__name__)
 
 @app.route("/")
 def welcome():
-    """List all available api routes."""
+    """List all available API routes."""
     return (
         f"Available Routes:<br/>"
-        f"/api/v1.0/names<br/>"
-        f"/api/v1.0/passengers"
+        f"/api/flights<br/>"
     )
 
-
-@app.route("/api/v1.0/names")
-def names():
+@app.route("/api/flights")
+def flights():
     # Create our session (link) from Python to the DB
     session = Session(engine)
 
-    """Return a list of all passenger names"""
-    # Query all passengers
-    results = session.query(Passenger.name).all()
+    """Return a list of flight data including the date, departure hour, and destination"""
+    # Query all flights
+    results = session.query(Flight.FL_DATE, Flight.DEP_HOUR, Flight.DEST, Flight.TAXI_OUT).all()
 
     session.close()
 
-    # Convert list of tuples into normal list
-    all_names = list(np.ravel(results))
+    # Create a dictionary from the row data and append to a list of all_flights
+    all_flights = []
+    for FL_DATE, DEP_HOUR, DEST,TAXI_OUT in results:
+        flight_dict = {}
+        flight_dict["FL_DATE"] = FL_DATE
+        flight_dict["DEP_HOUR"] = DEP_HOUR
+        flight_dict["DEST"] = DEST
+        flight_dict["TAXI_OUT"] = TAXI_OUT
+        all_flights.append(flight_dict)
 
-    return jsonify(all_names)
-
-
-@app.route("/api/v1.0/passengers")
-def passengers():
-    # Create our session (link) from Python to the DB
-    session = Session(engine)
-
-    """Return a list of passenger data including the name, age, and sex of each passenger"""
-    # Query all passengers
-    results = session.query(Passenger.name, Passenger.age, Passenger.sex).all()
-
-    session.close()
-
-    # Create a dictionary from the row data and append to a list of all_passengers
-    all_passengers = []
-    for name, age, sex in results:
-        passenger_dict = {}
-        passenger_dict["name"] = name
-        passenger_dict["age"] = age
-        passenger_dict["sex"] = sex
-        all_passengers.append(passenger_dict)
-
-    return jsonify(all_passengers)
+    return jsonify(all_flights)
 
 
 if __name__ == '__main__':
