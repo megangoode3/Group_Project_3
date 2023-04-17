54de8bd639cf5cac8c329231fbe53c53f18b3644
Bennett <bennettnorth@gmail.com>
2023-04-16 19:59:20 -0500

    HTML edits and API

--- app.py
+++ app.py
@@ -1,18 +1,17 @@
-import numpy as np
-
+# Python SQL toolkit and Object Relational Mapper
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
-from sqlalchemy import create_engine, func
-
-from flask import Flask, jsonify
-
+from sqlalchemy import create_engine, inspect
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
+import sqlite3
 
 #################################################
 # Database Setup
 #################################################
-engine = create_engine("sqlite:///flight_db")
-
+engine = create_engine('sqlite:///flight_db.sqlite', echo=False)
 # reflect an existing database into a new model
 Base = automap_base()
 # reflect the tables
@@ -26,11 +25,6 @@ Flight = Base.classes.flight_db
 #################################################
 app = Flask(__name__)
 
-
-#################################################
-# Flask Routes
-#################################################
-
 @app.route("/")
 def welcome():
     """List all available API routes."""
@@ -40,27 +34,39 @@ def welcome():
     )
 
 @app.route("/api/flights")
-def flights():
+def flight_data():
     # Create our session (link) from Python to the DB
     session = Session(engine)
+    # Query all flight data
+    results = session.query(Flight.FL_DATE, Flight.DEP_HOUR, Flight.OP_UNIQUE_CARRIER, Flight.OP_CARRIER_FL_NUM, Flight.TAIL_NUM, Flight.ORIGIN, Flight.DEST, Flight.DEP_TIME, Flight.CRS_DEP_TIME, Flight.TAXI_OUT, Flight.DEP_DELAY, Flight.AIR_TIME, Flight.CANCELLED, Flight.YEAR_OF_MANUFACTURE, Flight.MANUFACTURER, Flight.ICAO_TYPE, Flight.ACTIVE_WEATHER).all()
 
-    """Return a list of flight data including the date, departure hour, and destination"""
-    # Query all flights
-    results = session.query(Flight.FL_DATE, Flight.DEP_HOUR, Flight.DEST, Flight.TAXI_OUT).all()
-
-    session.close()
-
-    # Create a dictionary from the row data and append to a list of all_flights
-    all_flights = []
-    for FL_DATE, DEP_HOUR, DEST,TAXI_OUT in results:
+    # Create a dictionary from the row data and append to a list of all_passengers
+    all_flight_data = []
+    for FL_DATE, DEP_HOUR, OP_UNIQUE_CARRIER, OP_CARRIER_FL_NUM, TAIL_NUM, ORIGIN, DEST, DEP_TIME, CRS_DEP_TIME, TAXI_OUT, DEP_DELAY, AIR_TIME, CANCELLED, YEAR_OF_MANUFACTURE, MANUFACTURER, ICAO_TYPE, ACTIVE_WEATHER in results:
         flight_dict = {}
         flight_dict["FL_DATE"] = FL_DATE
         flight_dict["DEP_HOUR"] = DEP_HOUR
+        flight_dict["OP_UNIQUE_CARRIER"] = OP_UNIQUE_CARRIER
+        flight_dict["OP_CARRIER_FL_NUM"] = OP_CARRIER_FL_NUM
+        flight_dict["TAIL_NUM"] = TAIL_NUM
+        flight_dict["ORIGIN"] = ORIGIN
         flight_dict["DEST"] = DEST
+        flight_dict["DEP_TIME"] = DEP_TIME
+        flight_dict["CRS_DEP_TIME"] = CRS_DEP_TIME
         flight_dict["TAXI_OUT"] = TAXI_OUT
-        all_flights.append(flight_dict)
+        flight_dict["DEP_DELAY"] = DEP_DELAY
+        flight_dict["AIR_TIME"] = AIR_TIME
+        flight_dict["CANCELLED"] = CANCELLED
+        flight_dict["YEAR_OF_MANUFACTURE"] = YEAR_OF_MANUFACTURE
+        flight_dict["MANUFACTURER"] = MANUFACTURER
+        flight_dict["ICAO_TYPE"] = ICAO_TYPE
+        flight_dict["ACTIVE_WEATHER"] = ACTIVE_WEATHER
+        all_flight_data.append(flight_dict)
 
-    return jsonify(all_flights)
+    return jsonify(all_flight_data)
+
+
+
 
 
 if __name__ == '__main__':
